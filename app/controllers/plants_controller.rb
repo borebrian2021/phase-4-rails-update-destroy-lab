01fe1fbe48614240f1537eeca92afae49a2f6f52
borebrian2021 <bkimutai2021@gmail.com>
2022-09-28 01:46:09 +0300

    commit tree fbd7ba6d1314450ed2a6238c6c7edd0ecf74f8a5

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound , with: :not_found
   # GET /plants
   def index
     plants = Plant.all
@@ -18,9 +18,29 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant=find_plant
+    plant.update(is_in_stock: params[:is_in_stock])
+    render json:plant
+  end
+  def destroy
+    plant=find_plant
+    plant.destroy
+
+  end
   private
+  def not_found
+  render json:{error:"Plant not found"}, status: :not_found 
+  end
+
+
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+ 
+  def find_plant
+    Plant.find(params[:id])
   end
 end
